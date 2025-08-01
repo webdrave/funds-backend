f5fc9531bf34dd3a5dac0b52061ba5c81a11ff99
Aditya Bansal <adityabansalsafe@gmail.com>
2025-08-01 17:03:47 +0530

    fix

--- src/routes/analytics.routes.ts
+++ src/routes/analytics.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import { getLoanDsaActivityStats, getPlanPopularityStats, getTopUsersStats } from "../controllers/analytics.controller";
+import { authenticate, requireSuperadmin } from "../middleware";
 
 const analyticsRoutes = Router();
 
-analyticsRoutes.get("/dsa-activity-stats", getLoanDsaActivityStats);
-analyticsRoutes.get("/plan-popularity", getPlanPopularityStats)
-analyticsRoutes.get("/top-users", getTopUsersStats)
+analyticsRoutes.get("/dsa-activity-stats",authenticate,requireSuperadmin, getLoanDsaActivityStats);
+analyticsRoutes.get("/plan-popularity",authenticate,requireSuperadmin, getPlanPopularityStats)
+analyticsRoutes.get("/top-users", authenticate, requireSuperadmin, getTopUsersStats)
 
 export default analyticsRoutes;
